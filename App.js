48150f4f8e1a39c302ab035cdd09d3706f177810
argemirojr <jr.rj.junior@gmail.com>
2023-10-10 00:39:06 -0300

    form created

--- App.js
+++ App.js
@@ -1,11 +1,35 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.inputContainer}>
+        <Text style={styles.text}>Nome:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={'Nome'}
+        />  
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.text}>Saldo:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={'Saldo'}
+        />  
+      </View>
+
+      <View style={styles.inputContainer}>
+        <Text style={styles.text}>CPF:</Text>
+        <TextInput
+          style={styles.input}
+          placeholder={'CPF'}
+        />  
+      </View>
+
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Cadastrar</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -14,7 +38,31 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 20,
   },
+  input: {
+    borderWidth: 1,
+    paddingVertical: 8,
+    paddingHorizontal: 11,
+    borderRadius: 10,
+  },
+  inputContainer: {
+    marginBottom: 10,
+  },
+  button: {
+    marginTop: 10,
+    backgroundColor: '#88f',
+    padding: 10,
+    borderRadius: 10,
+    alignItems: 'center',
+    width: 110,
+  },
+  buttonText: {
+    color: 'white',
+    fontWeight: 'bold',
+    fontSize: 18,
+  },
+  text: {
+    marginBottom: 3,
+  }
 });
